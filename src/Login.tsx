12f88d45f3da178b25e67baa4ba03b2ef01636d2
古垣　博光 <h.f.appmaker@gmail.com>
2024-04-29 20:01:24 +0900

    ログイン画面モック作成

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,21 +1,23 @@
 import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField'
+import Box from '@mui/material/Box'
 import './Login.css'
 import { usePostsQuery } from './api/getValue'
 
 function Login() {
   const result = usePostsQuery();
-  if(result.isError){
+  if (result.isError) {
     return (
       <>
-      <h3>error</h3>
+        <h3>error</h3>
       </>
     )
   }
 
-  if(result.isLoading){
+  if (result.isLoading) {
     return (
       <>
-      <h3>loading</h3>
+        <h3>loading</h3>
       </>
     )
   }
@@ -24,7 +26,16 @@ function Login() {
 
   return (
     <>
-      <Button variant="contained">ログイン</Button>
+      <Box sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        '& .MuiTextField-root': { width: '35ch' },
+        button: { m: 1 },
+      }}>
+        <TextField id="input-mailaddress" label="メールアドレス" variant="outlined" margin="normal"/>
+        <TextField id="input-password" label="パスワード" variant="outlined" type='password' />
+        <Button variant="contained" >ログイン</Button>
+      </Box>
     </>
   )
 }
--- src/api/getValue.ts
+++ src/api/getValue.ts
@@ -1,21 +1,21 @@
- import { useQuery } from "react-query"
- import axios from "axios"
- // レスポンスの型定義
-  interface Posts {
-    countUp: number;
-  }
+import { useQuery } from "react-query"
+import axios from "axios"
+// レスポンスの型定義
+interface Posts {
+  countUp: number;
+}
 
-  type PostsGetResponse = Posts[];
+type PostsGetResponse = Posts[];
 
-  // Promiseを返す関数
-  const fetchPosts = async () => {
-    const { data } = await axios.request<PostsGetResponse>({
-      url: "http://localhost:8000/ping",
-      method: "GET",
-    })
-    return data
-  }
+// Promiseを返す関数
+const fetchPosts = async () => {
+  const { data } = await axios.request<PostsGetResponse>({
+    url: "http://localhost:8000/ping",
+    method: "GET",
+  })
+  return data
+}
 
 export const usePostsQuery = () => {
-return useQuery(["posts"], fetchPosts);
+  return useQuery(["posts"], fetchPosts);
 };
